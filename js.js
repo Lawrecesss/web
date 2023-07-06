76f02f6c5b56f1f68afe933dfd66ecd45e8fa08a
Lawrence <lhshein14@gmail.com>
2023-07-06 10:40:47 +0630

    js

--- js.js
+++ js.js
@@ -1,8 +1,18 @@
-let a = "red";
-let b = "blue";
+checkSpeed(70)
 
-a = b;
-b = a;
-
-console.log(a);
-console.log(b);
+function checkSpeed(speed){
+    let point = 0
+    if (speed < 73){
+        console.log('Ok');
+    }
+    else if (speed > 70){
+        for (speed; (speed - 5) >= 70; speed -= 5){
+            point += 1;
+            console.log("Point = ", point);
+            if (point > 12){
+                console.log("License is supended!!")
+            }
+        }
+        
+    }
+}
